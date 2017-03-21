28639e085be207b392d276f045057587b1357728
Edwin Lee <leeed2001@gmail.com>
2017-03-20 20:18:43 -0600

    Initial addition of consulting page

--- main/settings.py
+++ main/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cv.apps.CvConfig',
     'pokecards.apps.PokecardsConfig',
+    'consulting.apps.ConsultingConfig',
 ]
 
 MIDDLEWARE_CLASSES = [
