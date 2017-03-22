e869a4b216103f64fa73bbbd33edc29f3419588b
Edwin Lee <leeed2001@gmail.com>
2017-03-22 08:10:53 -0600

    Initial status update page

--- consulting/views.py
+++ consulting/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'consulting/index.html')
+
+
+def status(request):
+    return render(request, 'consulting/status.html')
