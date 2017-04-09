ab471d6068076c0769a0a2c7740772406bd0a4bf
Edwin Lee <leeed2001@gmail.com>
2017-04-09 06:22:43 -0600

    Pulling out pokecards

--- main/settings.py
+++ main/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'cv.apps.CvConfig',
-    'pokecards.apps.PokecardsConfig',
     'consulting.apps.ConsultingConfig',
     'family.apps.FamilyConfig',
 ]
