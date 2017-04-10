bf0f0a75d3ea11c80c6886d3125a01eed6ace553
Edwin Lee <leeed2001@gmail.com>
2017-04-10 08:49:06 -0600

    Add flake8 success

--- cv/urls.py
+++ cv/urls.py
@@ -5,14 +5,13 @@ from django.conf.urls.static import static
 from . import views
 
 app_name = 'cv'
-urlpatterns = [
-                  url(r'^$', views.index, name='index'),
-                  url(r'^publications/', views.publications, name='publications'),
-                  url(r'^education/', views.education, name='education'),
-                  url(r'^experience/', views.experience, name='experience'),
-                  url(r'^skills/', views.skills, name='skills'),
-                  url(r'^memberships/', views.memberships, name='memberships'),
-                  url(r'^projects/', views.projects, name='projects'),
-                  url(r'^html/', views.html, name='html'),
-                  url(r'^pdf/', views.pdf, name='pdf'),
-              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns = [url(r'^$', views.index, name='index'),
+               url(r'^publications/', views.publications, name='publications'),
+               url(r'^education/', views.education, name='education'),
+               url(r'^experience/', views.experience, name='experience'),
+               url(r'^skills/', views.skills, name='skills'),
+               url(r'^memberships/', views.memberships, name='memberships'),
+               url(r'^projects/', views.projects, name='projects'),
+               url(r'^html/', views.html, name='html'),
+               url(r'^pdf/', views.pdf, name='pdf'),
+               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- main/settings.py
+++ main/settings.py
@@ -57,8 +57,7 @@ ROOT_URLCONF = 'main.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates'), 'templates']
-        ,
+        'DIRS': [os.path.join(BASE_DIR, 'templates'), 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
