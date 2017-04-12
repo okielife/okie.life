f7c1b66211a8ec3d3a8505eba20d7a5d370a9116
Edwin Lee <leeed2001@gmail.com>
2017-04-11 19:53:58 -0600

    Try to fix settings module issue

--- main/wsgi.py
+++ main/wsgi.py
@@ -12,7 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cv_okie_life_django.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
